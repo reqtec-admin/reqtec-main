4e1fc583db989fd4bcae41880aa6f85480540dc2
Aaron <aarongilliland@gmail.com>
2025-05-15 10:25:10 -0500

    logo color chaange

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,17 +1,11 @@
 import particles from '@/public/images/particles.gif'
-import logo from '@/public/images/REQtec-logo.png'
+import logo from '@/public/images/REQtec-logo-white.png'
 import Head from 'next/head'
 import Modal from './modal'
 
 export default function Hero() {
   return (
     <section>
-
-        {/* Inject the script into the <head> */}
-        <Head>
-        <script src="http://localhost:8097" />
-      </Head>
-      {/* Modal with full-width, no padding */}
       <Modal 
         bgImage={particles}
         bgImageWidth={1200}
@@ -32,9 +26,6 @@ export default function Hero() {
             <div data-aos="fade-up" data-aos-delay="400">
               <a className="btn text-white bg-teal-600 hover:bg-teal-700 w-full mb-4 sm:w-auto sm:mb-0" href="mailto:info@reqtec.com">Contact Us</a>
             </div>
-            {/* <div data-aos="fade-up" data-aos-delay="600">
-              <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
-            </div> */}
           </div>
         </div>
       </div>
